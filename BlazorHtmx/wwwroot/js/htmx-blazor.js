2fed529a64fce1d680dfb9b02c3b6de3ae936b79
Sean Terry <seanterry@outlook.com>
2026-08-29 21:35:50 -0400

    Add Khalid's enhanved nav script

---
+++ BlazorHtmx/wwwroot/js/htmx-blazor.js
@@ -0,0 +1,5 @@
+// An enhanced load allows users to navigate between different pages
+Blazor.addEventListener("enhancedload", function () {
+  // HTMX need to reprocess any htmx tags because of enhanced loading
+  htmx.process(document.body);
+});
